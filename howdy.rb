93d35b388b87c7b649bd215676617f0ab978a03c
Aren Mizuno <166631743+arenmizuno@users.noreply.github.com>
2025-01-21 23:24:19 +0000

    add

--- howdy.rb
+++ howdy.rb
@@ -1,6 +1,15 @@
 pp "See ya later!"
 require "active_support/all"
+require "activesupport"
+require "awesome_print"
+require "pry-byebug"
 
 
 pp -103.ordinalize
 pp "table".pluralize
+
+pp "What's your name?"
+
+their_name = gets.chomp
+pp "Hello, " + their_name + "!"
+puts "Hello, " + their_name + "!"
